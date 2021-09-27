f0f1212def7dd3b0e514fc99379ada386f515b7f
Viacheslav Kozlov <slavick.cv@gmail.com>
2021-09-27 20:51:52 +0200

    Updates

--- static/js/main.230a2464.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);c(1);var n=c(3),r=c.n(n),a=c(0),s=function(){return Object(a.jsx)(a.Fragment,{})};r.a.render(Object(a.jsx)(a.Fragment,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.230a2464.chunk.js.map
